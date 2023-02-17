e0e47fecc7f6bd1688f1be6a923dda68501a5dd1
Jesse Otu <chibuzorotu98@gmail.com>
2023-02-17 10:58:30 +0000

    Reset 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /* more headers go here */
 
-/*
+/**
  * main - Entry point
  *
  * Return: print whether an integer stored in a \
@@ -11,21 +11,21 @@ variable is positive or negative
 */
 int main(void)
 {
-int n;
+  int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-write(1, "is positive\n", 12);
-}
-else if (n == 0)
-{
-write(1, "is zero\n", 8);
-}
-else
-{
-write(1, "is negative\n", 12);
-}
-return (0);
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  if (n > 0)
+  {
+    printf("%d is positive\n", n);
+  }
+  else if (n == 0)
+  {
+    printf("%d is zero\n", n);
+  }
+  else
+  {
+    printf("%d is negative\n", n);
+  }
+  return (0);
 }
