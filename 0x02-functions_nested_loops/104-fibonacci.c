64493e2fed2f5b4bbc7b8f3ab371e0f8f0d2607a
Jesse Otu <chibuzorotu98@gmail.com>
2023-02-21 18:15:36 +0000

    Modified 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,25 +2,26 @@
 #include <stdio.h>
 
 /**
-* main - prints the first 98 Fibonacci numbers, starting with 1 and 2\
-, followed by a new line
+* main - prints the first 98 Fibonacci numbers, starting with 1 and 2, \
+followed by a new line
 *
 * Return: 0
 */
 int main(void)
 {
+int i;
 long int a, b, c;
 
 a = 1;
 b = 2;
 printf("%ld, %ld, ", a, b);
-while (c < 4000000)
+for (i = 0; i < 96; i++)
 {
 c = a + b;
 a = b;
 b = c;
 printf("%ld", c);
-if (c < 4000000)
+if (i < 95)
 printf(", ");
 }
 printf("\n");
