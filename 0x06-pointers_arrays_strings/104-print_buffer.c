d2084a07508e40c7a869e972f22649afdde2c48f
Jesse Otu <chibuzorotu98@gmail.com>
2023-03-02 16:10:05 +0000

    Added 104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of the buffer
+ */
+void print_buffer(char *b, int size)
+{
+int i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+
+i = j = k = l = m = n = o = p = q = r = s = t = u = v = w = x = y = z = 0;
+if (size <= 0)
+printf("\n");
+else
+{
+while (i < size)
+{
+printf("%08x: ", i);
+j = 0;
+while (j < 10)
+{
+if (i + j < size)
+printf("%02x", b[i + j]);
+else
+printf("  ");
+if (j % 2 != 0)
+printf(" ");
+j++;
+}
+printf("; ");
+k = 0;
+while (k < 10)
+{
+if (i + k < size)
+{
+l = b[i + k];
+if (l >= 32 && l <= 126)
+printf("%c", l);
+else
+printf(".");
+}
+else
+printf(" ");
+k++;
+}
+printf("\n");
+i += 10;
+}
+}
+}
