adb3368a5ec88cd8288b43a8bc12c36e9f42590e
Jesse Otu <chibuzorotu98@gmail.com>
2023-03-08 18:48:46 +0000

    Modified 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,6 +17,19 @@ return (palindrome_helper(s + 1, len - 2));
 }
 
 /**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to check
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
+
+/**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to check
  *
