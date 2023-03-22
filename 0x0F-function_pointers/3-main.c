c2802f193a147281a480cdf5afd7502785ad9ba3
Jesse Otu <chibuzorotu98@gmail.com>
2023-03-22 17:06:27 +0000

    modified 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,13 +17,7 @@ printf("Error\n");
 exit(98);
 }
 
-if (argv[2][1] != '\0')
-{
-printf("Error\n");
-exit(99);
-}
-
-if (get_op_func(argv[2]) == NULL)
+if (argv[2][1] != '\0' || get_op_func(argv[2]) == NULL)
 {
 printf("Error\n");
 exit(99);
