2149b570c53f0fef7b169d2146c3d22aa02b0259
Jesse Otu <chibuzorotu98@gmail.com>
2023-03-23 17:31:57 +0000

    Modified 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,45 +2,6 @@
 #include <stdio.h>
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- * Return: void
- */
-void print_all(const char * const format, ...)
-{
-va_list ap;
-unsigned int i = 0, j;
-char *str;
-
-print_t print[] = {
-{"c", print_char},
-{"i", print_int},
-{"f", print_float},
-{"s", print_string},
-{NULL, NULL}
-};
-
-va_start(ap, format);
-while (format != NULL && format[i] != '\0')
-{
-j = 0;
-while (print[j].type != NULL)
-{
-if (format[i] == *(print[j].type))
-{
-printf("%s", (i > 0) ? ", " : "");
-print[j].f(ap);
-break;
-}
-j++;
-}
-i++;
-}
-printf("\n");
-va_end(ap);
-}
-
-/**
  * print_char - prints a char
  * @ap: list of arguments
  * Return: void
@@ -83,4 +44,43 @@ str = va_arg(ap, char *);
 if (str == NULL)
 str = "(nil)";
 printf("%s", str);
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+va_list ap;
+unsigned int i = 0, j;
+char *str;
+
+print_t print[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_string},
+{NULL, NULL}
+};
+
+va_start(ap, format);
+while (format != NULL && format[i] != '\0')
+{
+j = 0;
+while (print[j].type != NULL)
+{
+if (format[i] == *(print[j].type))
+{
+printf("%s", (i > 0) ? ", " : "");
+print[j].f(ap);
+break;
+}
+j++;
+}
+i++;
+}
+printf("\n");
+va_end(ap);
 }
