344bd801c08ef30b64aa12e33af4429b75442304
Jesse Otu <chibuzorotu98@gmail.com>
2023-04-09 20:57:21 +0000

    Modified 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,12 +31,12 @@ return (-1);
 
 if (close(fd_from))
 return (0);
-else if (close(fd_from) == -1)
+else if (!close(fd_from))
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 
 if (close(fd_to))
 return (0);
-else if (close(fd_to) == -1)
+else if (!close(fd_to))
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 
 return (1);
