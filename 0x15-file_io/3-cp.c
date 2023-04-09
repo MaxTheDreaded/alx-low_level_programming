475acc7e850e7b5fa69ec4c4eff2d8aebc59a3f8
Jesse Otu <chibuzorotu98@gmail.com>
2023-04-09 21:14:51 +0000

    Modified 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * error_msg - prints error messages
+ * @err: error number
+ * @file: file name
+ */
+void error_msg(int err, char *file)
+{
+switch (err)
+{
+case 97:
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+case 98:
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+exit(98);
+case 99:
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+exit(99);
+case 100:
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", err);
+exit(100);
+}
+}
+
+/**
  * copier - copies the content of a file to another file
  * @file_from: file to copy from
  * @file_to: file to copy to
@@ -14,41 +38,44 @@ char buf[1024];
 fd_from = open(file_from, O_RDONLY);
 if (fd_from == -1)
 return (-1);
-
-if (access(file_to, W_OK) == 0)
-{
 fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
-return (-1);
-}
-else
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-exit(99);
+close(fd_from);
+error_msg(100, NULL);
 }
-
-while ((r = read(fd_from, buf, 1024)) > 0)
+r = read(fd_from, buf, 1024);
+if (r == -1)
+{
+close(fd_from);
+close(fd_to);
+error_msg(98, (char *)file_from);
+}
+while (r > 0)
 {
 w = write(fd_to, buf, r);
 if (w == -1)
-return (-1);
+{
+close(fd_from);
+close(fd_to);
+error_msg(99, (char *)file_to);
 }
-
+r = read(fd_from, buf, 1024);
 if (r == -1)
-return (-1);
-
-if (close(fd_from))
-return (0);
-else if (!close(fd_from))
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-
-if (close(fd_to))
-return (0);
-else if (!close(fd_to))
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+{
+close(fd_from);
+close(fd_to);
+error_msg(98, (char *)file_from);
+}
+}
+if (close(fd_from) == -1)
+error_msg(100, NULL);
+if (close(fd_to) == -1)
+error_msg(100, NULL);
 
 return (1);
 }
+
 
 /**
  * main - copies the content of a file to another file
@@ -58,14 +85,16 @@ return (1);
  */
 int main(int argc, char *argv[])
 {
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-}
+int c;
 
-if (copier(argv[1], argv[2]) == -1)
+if (argc != 3)
+error_msg(97, NULL);
+
+c = copier(argv[1], argv[2]);
+if (c == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+error_msg(98, argv[1]);
+error_msg(99, argv[2]);
 }
 
 return (0);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,5 +14,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int copier(const char *file_from, const char *file_to);
+void error_msg(int err, char *file);
 
 #endif /* MAIN_H_ */
