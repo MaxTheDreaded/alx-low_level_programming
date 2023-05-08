b2a462d56371592578861d73afcf842b7288f972
Jesse Otu <chibuzorotu98@gmail.com>
2023-05-08 18:49:22 +0000

    Added 3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int open_from(char *file);
+int open_to(char *file);
+void copy_file(int fd_from, int fd_to);
 
 #endif /* MAIN_H */
