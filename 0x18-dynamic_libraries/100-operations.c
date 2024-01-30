c8f49d1f034fd494daccb9602a3b9fa83a3943a9
Jesse Otu <chibuzorotu98@gmail.com>
2024-01-30 01:07:06 +0000

    added 100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * _add - adds two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int _add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * _sub - subtracts two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+int _sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * _mul - multiplies two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+int _mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * _div - divides two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: quotient of a and b
+ */
+int _div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * _mod - divides two integers and returns the remainder
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a and b
+ */
+int _mod(int a, int b)
+{
+return (a % b);
+}
