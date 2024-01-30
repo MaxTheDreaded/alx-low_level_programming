3f3e8f5efdd0f3d0eb8fcf4903244fa73daa2027
Jesse Otu <chibuzorotu98@gmail.com>
2024-01-30 09:47:20 +0000

    modifications

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,61 +1,61 @@
 #include "main.h"
 
 /**
- * _add - adds two integers and returns the result
+ * add - adds two integers and returns the result
  * @a: first integer
  * @b: second integer
  *
  * Return: sum of a and b
  */
-int _add(int a, int b)
+int add(int a, int b)
 {
 return (a + b);
 }
 
 /**
- * _sub - subtracts two integers and returns the result
+ * sub - subtracts two integers and returns the result
  * @a: first integer
  * @b: second integer
  *
  * Return: difference of a and b
  */
-int _sub(int a, int b)
+int sub(int a, int b)
 {
 return (a - b);
 }
 
 /**
- * _mul - multiplies two integers and returns the result
+ * mul - multiplies two integers and returns the result
  * @a: first integer
  * @b: second integer
  *
  * Return: product of a and b
  */
-int _mul(int a, int b)
+int mul(int a, int b)
 {
 return (a * b);
 }
 
 /**
- * _div - divides two integers and returns the result
+ * div - divides two integers and returns the result
  * @a: first integer
  * @b: second integer
  *
  * Return: quotient of a and b
  */
-int _div(int a, int b)
+int div(int a, int b)
 {
 return (a / b);
 }
 
 /**
- * _mod - divides two integers and returns the remainder
+ * mod - divides two integers and returns the remainder
  * @a: first integer
  * @b: second integer
  *
  * Return: remainder of a and b
  */
-int _mod(int a, int b)
+int mod(int a, int b)
 {
 return (a % b);
 }
