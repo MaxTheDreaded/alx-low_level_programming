c555bc384e28e360ec3bd7e789fcf74f1c1ba0da
Jesse Otu <chibuzorotu98@gmail.com>
2024-02-21 23:45:48 +0000

    updated 5 again

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,6 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *temp;
+	int first_pair = 1;
 
 	if (ht == NULL)
 		return;
@@ -19,10 +20,13 @@ void hash_table_print(const hash_table_t *ht)
 		temp = ht->array[i];
 		while (temp != NULL)
 		{
+			if (!first_pair) {
+				printf(", ");
+			} else {
+				first_pair = 0;
+			}
 			printf("'%s': '%s'", temp->key, temp->value);
 			temp = temp->next;
-			if (temp != NULL)
-				printf(", ");
 		}
 	}
 	printf("}\n");
